b12da3ae7109915ae1b42703efa5ed83fd691131
Jasper Lievisse Adriaanse <jasper@humppa.nl>
2014-10-05 09:36:09 +0200

    port release version bump

--- Portroach/Const.pm
+++ Portroach/Const.pm
@@ -42,10 +42,10 @@ our @ISA = qw(Exporter);
 
 use constant {
 	APPNAME     		=> 'portroach',
-	APPVER      		=> '1.2.0',
+	APPVER      		=> '1.2.1',
 	AUTHOR      		=> 'Shaun Amott and Jasper Lievisse Adriaanse',
 
-	USER_AGENT  		=> 'portroach/1.2.0',
+	USER_AGENT  		=> 'portroach/1.2.1',
 
 	DB_VERSION  		=> 2014050901,
 
