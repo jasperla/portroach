22b74fa5fe9483674d32ed3786884d1770ced7d4
Antoine Jacoutot <ajacoutot@gnome.org>
2015-04-15 18:59:15 +0200

    Sync version, we are working on 2.0.4 now.

--- Portroach/Const.pm
+++ Portroach/Const.pm
@@ -35,7 +35,7 @@ require 5.006;
 
 our @ISA = qw(Exporter);
 
-our $VERSION = '2.0.2';
+our $VERSION = '2.0.4';
 
 #------------------------------------------------------------------------------
 # Constants
@@ -43,7 +43,7 @@ our $VERSION = '2.0.2';
 
 use constant {
 	APPNAME     		=> 'portroach',
-	APPVER      		=> '2.0.2',
+	APPVER      		=> '2.0.4',
 	AUTHOR      		=> 'Shaun Amott and Jasper Lievisse Adriaanse',
 
 	USER_AGENT  		=> "portroach/${VERSION}",
