281fdd3f83b9c6efb72d09ced02a51667f420b08
Jasper Lievisse Adriaanse <jasper@humppa.nl>
2015-07-26 23:28:07 +0200

    post-release version bump

--- Portroach/Const.pm
+++ Portroach/Const.pm
@@ -35,7 +35,7 @@ require 5.006;
 
 our @ISA = qw(Exporter);
 
-our $VERSION = '2.0.6';
+our $VERSION = '2.0.7';
 
 #------------------------------------------------------------------------------
 # Constants
@@ -43,7 +43,7 @@ our $VERSION = '2.0.6';
 
 use constant {
 	APPNAME     		=> 'portroach',
-	APPVER      		=> '2.0.6',
+	APPVER      		=> '2.0.7',
 	AUTHOR      		=> 'Shaun Amott and Jasper Lievisse Adriaanse',
 
 	USER_AGENT  		=> "portroach/${VERSION}",
